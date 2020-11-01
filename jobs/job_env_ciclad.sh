afdd20df1e62ae46f236216e6d1554fab14315c2
Stephane SENESI <senesi@meteo.fr>
2020-11-01 15:11:13 +0100

    adapt job_env_ciclad.sh to new conda environment /modfs/modtools/miniconda2

--- jobs/job_env_ciclad.sh
+++ jobs/job_env_ciclad.sh
@@ -6,11 +6,13 @@
 
 module load ncl/6.6.2 cdo/1.9 netcdf4/4.3.3.1-gfortran TeX-live
 #
-conda_env=/modfs/ipslfs/dods/jservon/miniconda
+#conda_env=/thredds/ipsl/jservon/miniconda
+conda_env=/modfs/modtools/miniconda2
 CLIMAF=/home/ssenesi/climaf_installs/climaf_running
 #CLIMAF=/home/ssenesi/climaf_installs/climaf_V2.0.0
 #
-export PATH=${conda_env}/envs/analyse_env_2.7/bin:${conda_env}/bin:$CLIMAF/bin:$PATH 
+#export PATH=${conda_env}/envs/analyse_env_2.7/bin:${conda_env}/bin:$CLIMAF/bin:$PATH 
+export PATH=${conda_env}/envs/analyse_2.7/bin:${conda_env}/bin:$CLIMAF/bin:$PATH 
 export LD_LIBRARY_PATH=${conda_env}/lib:$LD_LIBRARY_PATH
 export PYTHONPATH=$CLIMAF:$PYTHONPATH
 #
